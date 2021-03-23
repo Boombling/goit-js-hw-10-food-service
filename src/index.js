ae2763f2a57173ea66291c69700c8db37ef6c4cb
Dmytro Filius <mindertan@gmail.com>
2021-03-24 01:12:40 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ const menuRef = document.querySelector('.js-menu');
 const cardsMenu = createMenuCardList(menu);
 const switchTheme = document.querySelector('.theme-switch__track');
 const bodyRef = document.querySelector('body');
+// const theme = JSON.parse(localStorage.getItem('theme'));
 
 menuRef.insertAdjacentHTML('beforeend', cardsMenu)
 function createMenuCardList(menu) {
@@ -16,7 +17,42 @@ switchTheme.addEventListener('click', switcher)
 bodyRef.classList.add('light-theme');
 function switcher(evt) {
     if (evt.target) {
-        bodyRef.classList.toggle('light-theme')
-        bodyRef.classList.toggle('dark-theme')
+        bodyRef.classList.toggle('light-theme');
+        bodyRef.classList.toggle('dark-theme');
+        // localStorage.setItem('theme', 'dark');
     }
+    
+    // addLightTheme(evt)
+    // addDarkTheme(evt)
+    // themes (evt)
 }
+
+// localStorage.setItem('theme', 'dark');
+// const settings = {
+//   theme: 'dark',
+//   isAuthenticated: true,
+// };
+
+// localStorage.setItem('settings', JSON.stringify(settings));
+
+// function addDarkTheme(evt) {
+//     if (evt.target) {
+//         bodyRef.classList.remove('light-theme')
+//         bodyRef.classList.add('dark-theme')
+//     }
+// }
+// function addLightTheme(evt) {
+//     if (bodyRef.classList === 'dark-theme') {
+//         bodyRef.classList.remove('dark-theme')
+//         bodyRef.classList.add('light-theme')
+//     }
+    
+// }
+// function themes() {
+    if (bodyRef !== 'dark-theme') {
+        localStorage.setItem('theme', true);
+}
+    else {
+        localStorage.setItem('theme', false);
+    }
+// }
